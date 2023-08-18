dcb3f627e43f3c426cba515823e534733f73a50c
Marcin Wirzeski <40631628+martut@users.noreply.github.com>
2023-08-18 19:02:10 +0200

    manipulating resource and validating input

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
 })
+    .AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
